d00f04bfd83183b0d015b2c2b1986337739062b6
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-03-28 22:07:50 -0300

    multi feature prediction

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as ervy from 'ervy'
 import * as tf from '@tensorflow/tfjs'
 
 type Row = {
@@ -46,20 +47,29 @@ const main = async () => {
     .csv('file://D:/GitHub/ml/data/telecom_users.csv')
     .toArray()) as Row[]
 
-  const features = data.map((v) => v.TotalCharges / 999)
+  const totalChargesMax = data
+    .map((v) => v.TotalCharges)
+    .sort((a, b) => b - a)[0]
+  const monthlyChargesMax = data
+    .map((v) => v.MonthlyCharges)
+    .sort((a, b) => b - a)[0]
+
+  const features = data.map((v) => [
+    v.TotalCharges / totalChargesMax,
+    v.MonthlyCharges / monthlyChargesMax,
+  ])
   const labels = data.map((v) => binary[v.Churn])
 
-  // const max = features.sort()[features.length - 1]
-
-  // console.log(max)
-
-  const xs = tf.tensor2d(features, [features.length, 1])
+  const xs = tf.tensor2d(features, [features.length, 2])
   const ys = tf.tensor2d(labels, [labels.length, 1])
 
   // define model
 
   const model = tf.sequential()
-  model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+
+  model.add(
+    tf.layers.dense({ units: 1, inputShape: [2], activation: 'sigmoid' })
+  )
 
   model.compile({
     loss: tf.losses.meanSquaredError,
@@ -81,43 +91,29 @@ const main = async () => {
 
   // predict
 
-  const predictData = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    100,
-    300,
-    600,
-    1000,
-    1300,
-    1600,
-    1900,
-    2500,
-    3000,
-    3500,
-    4000,
-    4500,
-    5000,
-    6000,
-    7000,
-    8000,
-    9000,
-  ].map((v) => v / 999)
+  // [8000, 2000] => 8000 in total, an old customer. 2000 monthly, became very expensive. should predict a churn
+  // [2000, 12] => 2000 in total, an average customer. 10 monthly, very cheap. low chances of a churn
 
-  const predictTensor = tf.tensor2d(predictData, [predictData.length, 1])
+  const predictData = [[2000, 10]].map((v) => [
+    v[0] / totalChargesMax,
+    v[1] / monthlyChargesMax,
+  ])
+
+  const predictTensor = tf.tensor2d(predictData, [predictData.length, 2])
 
   const predict = model.predict(predictTensor) as tf.Tensor
-  const res = predict.dataSync()
+  const res = Array.from(predict.dataSync())
   console.log(res)
-  console.log(Array.from(res).map((v) => binary[Math.round(v)]))
+  console.log(res.map((v) => binary[Math.round(v)]))
+
+  // console.log(
+  //   ervy.bullet(
+  //     predictData.map((v, k) => ({
+  //       key: (v * totalChargesMax).toString(),
+  //       value: res[k],
+  //     }))
+  //   )
+  // )
 }
 
 main()
